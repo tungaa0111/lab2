34631a616e3a1eafbf99519a73455be89f8e95c4
tungaa0111 <sodbayrtungaa@gmail.com>
2025-06-03 15:27:44 +0800

    Vehicle orgotgol.java

--- lab 2/1/Vehicle.java
+++ lab 2/1/Vehicle.java
@@ -1,15 +1,24 @@
-// Vehicle.java
 public class Vehicle {
     protected String model;
     protected double fuelEfficiency;
     protected double distanceTraveled;
     protected double maxSpeed;
+    protected double fuel; // Шинэ атрибут
 
-    public Vehicle(String model, double fuelEfficiency, double distanceTraveled, double maxSpeed) {
+    // Constructor-д fuel нэмсэн
+    public Vehicle(String model, double fuelEfficiency, double distanceTraveled, double maxSpeed, double fuel) {
         this.model = model;
         this.fuelEfficiency = fuelEfficiency;
         this.distanceTraveled = distanceTraveled;
         this.maxSpeed = maxSpeed;
+        this.fuel = fuel;
+    }
+
+    public void refuel(double amount) {
+        if (amount > 0) {
+            fuel += amount;
+            System.out.println(model + " refueled with " + amount + " gallons.");
+        }
     }
 
     public void displayInfo(String type) {
@@ -17,28 +26,9 @@ public class Vehicle {
         System.out.println("Fuel Efficiency: " + fuelEfficiency + " mpg");
         System.out.println("Distance Traveled: " + distanceTraveled + " miles");
         System.out.println("Max Speed: " + maxSpeed + " mph");
+        System.out.println("Fuel Remaining: " + fuel + " gallons"); // шинэ мөр
         System.out.println();
     }
 }
 
-// Car.java
-class Car extends Vehicle {
-    public Car(String model, double fuelEfficiency, double distanceTraveled, double maxSpeed) {
-        super(model, fuelEfficiency, distanceTraveled, maxSpeed);
-    }
 
-    public void display() {
-        super.displayInfo("Car");
-    }
-}
-
-// Truck.java
-class Truck extends Vehicle {
-    public Truck(String model, double fuelEfficiency, double distanceTraveled, double maxSpeed) {
-        super(model, fuelEfficiency, distanceTraveled, maxSpeed);
-    }
-
-    public void display() {
-        super.displayInfo("Truck");
-    }
-}
